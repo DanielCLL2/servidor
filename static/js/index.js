d9882afbfe9b0af9d892d573d678a359a60e5a78
DanielCLL2 <89862646+DanielCLL2@users.noreply.github.com>
2021-09-29 10:17:14 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -21,8 +21,6 @@ function LED1_Off(){
 
 
 
-
-
 // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
@@ -73,8 +71,24 @@ function LED1_Off(){
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
     var MensajeRecibido=message.payloadString;
-    var la=message.payloadString.split("-");
+    //var la=message.payloadString.split("-");
     document.getElementById("sensor").innerHTML=la[0];
     document.getElementById("sensor1").innerHTML=la[1];
 
   }
+
+function leerArchivo(e) {
+  var archivo = e.target.files[0];
+
+  var lector = new FileReader();
+  lector.onload = function(e) {
+    var contenido = e.target.result;
+    mostrarContenido(contenido);
+  };
+  lector.readAsText(archivo);
+}
+
+function mostrarContenido(contenido) {
+  document.getElementById('contenido-archivo').innerHTML=contenido;
+}
+document.getElementById('file-input').addEventListener('change', leerArchivo, false);
